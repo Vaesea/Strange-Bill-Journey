ac158e1e42ea3bd991c366ad66e174c8be1adeb4
EggsAndBakey <159406785+EggsAndBakey@users.noreply.github.com>
2024-07-27 23:33:46 +0100

    bill uses the 2nd jump sprite now!!!

--- src/objects/creatures/bill.gd
+++ src/objects/creatures/bill.gd
@@ -2,7 +2,8 @@ extends CharacterBody2D
 
 
 const SPEED = 300.0 # The speed.
-const JUMP_VELOCITY = -400.0 # Jump velocity.
+const RUNSPEED = 400.0 # The Run Speed
+const JUMP_VELOCITY = -600.0 # Jump velocity.
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
@@ -12,6 +13,7 @@ func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
+		$Sprite0.texture = load("res://data/images/objects/creatures/player/bill/jumpsprite_2.png")
 	
 	if Input.is_action_pressed("player_left"):
 		$Sprite0.flip_h = true
@@ -20,8 +22,11 @@ func _physics_process(delta):
 		$Sprite0.flip_h = false
 
 	# Handle jump.
-	if Input.is_action_just_pressed("player_jump") and is_on_floor():
+	if Input.is_action_pressed("player_jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+		
+	if is_on_floor():
+		$Sprite0.texture = load("res://data/images/objects/creatures/player/bill/sprite_0.png")
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions. This has already been done.
